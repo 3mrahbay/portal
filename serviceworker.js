6db59d0b4fa5ca586ab3c0cce689c89a19765461
3mrahbay <emrahby@gmail.com>
2026-07-20 22:49:25 +0300

    v29
    v29

--- serviceworker.js
+++ serviceworker.js
@@ -10,7 +10,7 @@
    - Her dağıtımda CACHE_VERSION'ı artır → eski cache otomatik silinir.
    ============================================================ */
 
-const CACHE_VERSION = "v28";
+const CACHE_VERSION = "v29";
 const CACHE_NAME = `bircicek-portal-${CACHE_VERSION}`;
 
 // Açılışta önceden cache'lenecek temel kabuk varlıkları
