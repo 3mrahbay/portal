2d55ebe9a88b6a1eb7413a1f1e5cb02ed6255d65
3mrahbay <emrahby@gmail.com>
2026-09-18 17:50:20 +0300

    Eğitim aşamalarını ortak kayıtla senkronize et

--- js/zeky-galeri-filigran-koprusu.js
+++ js/zeky-galeri-filigran-koprusu.js
@@ -4,7 +4,7 @@
 // Ayrintili veli egitim + gelismis ogretmen gozlem koprusu de bu ortak
 // son-yuklenen modul uzerinden devreye girer; buyuk index.html'e dokunulmaz.
 import './zeky-veli-egitim-koprusu.js?v=6';
-import './zeky-galeri-onay-egitim.js?v=3';
+import './zeky-galeri-onay-egitim.js?v=4';
 
 const KOPRU_ANAHTARI = '__zekyGaleriFiligranKoprusuV4';
 
